ef90b39c185fa859165a9ac9a8a8e4d3858629e0
lam126 <wangyi163liu@163.com>
2024-04-05 19:35:20 +0800

    modified:   Momo/__pycache__/ip.cpython-311.pyc 	modified:   Momo/ip.py 	modified:   Momo/ip.txt 	modified:   Momo/main.py 	modified:   Momo/momo_link.txt 	modified:   Momo/test.py

--- Momo/ip.py
+++ Momo/ip.py
@@ -4,18 +4,30 @@ import chardet
 from random import choice
 import asyncio
 import requests
+import sys
+import random
 from pyppeteer import launch
 from aiohttp import ClientSession, ClientTimeout, TCPConnector
 # import encodings.idna
 
 listIP = []  # 保存IP地址
-cnt_findNewIp = 0 # 每次新增50个有效ip
+cnt_findNewIp = 0 # 每次新增100个有效ip
 path = '/var/www/tools/momo-share/momo-share-proxy/Momo/ip.txt'  # 文件保存地址
 
 
 # 随机返回请求头
 async def getheaders():
     headers_list = [
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_5 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8L1",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8J2",
+        "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8J2 Twitter for iPhone",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_5 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8L1",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8J3",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_5 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8L1 Safari/6533.18.5",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J3 Safari/6533.18.5",
+        "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_5 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8L1",
+        "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Mobile/8J2",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
         "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
@@ -35,6 +47,19 @@ async def getheaders():
         "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
         "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; zh-cn) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; ja-jp) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; ja-jp) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; zh-cn) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; sv-se) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; ko-kr) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; ja-jp) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; it-it) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; fr-fr) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; es-es) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-us) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-gb) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; de-de) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36", ]
     headers = {'User-Agent': choice(headers_list)}
     return headers
@@ -44,10 +69,10 @@ async def getheaders():
 def clear_file():
     with open(path, 'r') as f_in:
         lines = f_in.readlines()
-        listIP = lines[:100]
+        listIP = lines[:300]
         f_in.close()
-        if len(lines)>200:
-            print('ip数量大于200，开始清除')
+        if len(lines)>500:
+            print('ip数量大于500，开始清除')
             with open(path, 'w', encoding='utf-8') as f:
                 f.truncate()
                 f.writelines(listIP)
@@ -57,8 +82,10 @@ def clear_file():
 async def record(text):
     global listIP
     with open(path, 'a', encoding='utf-8') as f:
+        text = text + '\n'
         if text not in listIP:
-            f.write(f'{text}\n')
+            listIP.append(text)
+            f.write(text)
 
 async def getHtmlWithRunJs(url):
     print('启动浏览器')
@@ -69,8 +96,7 @@ async def getHtmlWithRunJs(url):
     await page.setJavaScriptEnabled(enabled=True)
     # 设置请求头
     await page.setUserAgent(
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-        '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'
     )
     # 开启 隐藏 是selenium 让网站检测不到
     await page.evaluate('''() =>{ Object.defineProperties(navigator,{ webdriver:{ get: () => false } }) }''')
@@ -89,47 +115,24 @@ async def getHtmlWithRunJs(url):
 async def check_proxy(proxy,session):
     hd = await getheaders()  # 设置请求头
     sem = asyncio.Semaphore(50)  # 设置限制并发次数.
-    tout = ClientTimeout(total=5)  # 设置请求超时
     global cnt_findNewIp
     async with sem:
         try:
-            async with await session.get(url='https://www.baidu.com', headers=hd, proxy=proxy, timeout=tout) as response:
+            async with await session.get(url='https://www.baidu.com', headers=hd, proxy=proxy, timeout=5) as response:
                 # 如果状态码为200，则代理可用
                 if response.status_code == 200:
                     print(f"代理 {proxy} 可用")
                     await record(proxy)
                     cnt_findNewIp += 1
-                    if cnt_findNewIp>50:
+                    if cnt_findNewIp>100:
                         print('本次新增有效ip',cnt_findNewIp)
-                        exit(0)
+                        #sys.exit(0)
                 else:
-                    print(f"代理 {proxy} 返回状态码 {response.status_code}")
+                    #print(f"代理 {proxy} 返回状态码 {response.status_code}")
+                    pass
         except Exception as e:
-            print(f"代理 {proxy} 请求失败")
-
-    try:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            # 'Accept-Encoding': 'gzip, deflate',
-            'Connection': 'keep-alive',
-            # 'Cookie': 'BAIDUID=99C9E0BB02F9B2785867606169DC3B5E:FG=1; BIDUPSID=10718E3AD78C242F341870B1F3600FA0; PSTM=1697958277; BDUSS=NmYm11aWpCOS04UjF3LXdXMmh2OTBhYS1xclhkMm1BUEIwOUxIV0R5alN4V1ZsSVFBQUFBJCQAAAAAAAAAAAEAAAD1h7lexOa54tbQtcTOonhpYW8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANI4PmXSOD5lZH; H_PS_PSSID=40206_40217_40223_40283_40295_40287_40284_40318_40080_40364_40352_40302_40366_40377_40410_40416_40011; MCITY=-%3A; COOKIE_SESSION=0_0_0_0_0_0_0_0_0_0_0_0_0_1706876179%7C1%230_0_0_0_0_0_0_0_1706876179%7C1; H_WISE_SIDS=110085_281879_282466_292505_290994_293754_292166_294108_287701_287174_292123_292246_294359_294755_294743_294749_269892_295129_295083_289026_295396_295412_295772_291191; MSA_WH=1586_804; BAIDU_WISE_UID=wapp_1707828950783_758; newlogin=1',
-            'Upgrade-Insecure-Requests': '1',
-        }
-        # 使用代理发起请求，timeout参数设置超时时间
-        response = requests.get("http://www.baidu.com/", headers=headers, proxies={"http": proxy, "https": proxy}, timeout=1)
-        # 如果状态码为200，则代理可用
-        if response.status_code == 200:
-            print(f"代理 {proxy} 可用")
-            await record(proxy)
-            return True
-        else:
-            print(f"代理 {proxy} 返回状态码 {response.status_code}")
-            return False
-    except Exception as e:
-        print(f"代理 {proxy} 请求失败")
-        return False
+            #print(f"代理 {proxy} 请求失败")
+            pass
 
 
 async def check_proxtList(proxy_list):
@@ -145,28 +148,27 @@ async def create_aiohttp():
     # 实例化对象
     async with ClientSession() as session:
         task = [
-        #    asyncio.create_task(get_page('http://www.kxdaili.com/dailiip/2/1.html',mod=2, session=session)),
-            asyncio.create_task(get_page('https://www.zdaye.com/free/?ip=&adr=&checktime=3&sleep=2',mod=2, session=session)),
-        #    asyncio.create_task(get_page('https://www.kuaidaili.com/free', mod=2, session=session,isUseJs = 1)),
-        #    asyncio.create_task(get_page('https://www.kuaidaili.com/free/fps/', mod=2, session=session,isUseJs = 1)),
-        #    asyncio.create_task(get_page('http://www.66ip.cn/mo.php?sxb=&tqsl=50',mod=0, session=session)),
-        #    asyncio.create_task(get_page('https://www.89ip.cn',mod=1, session=session)),
-        #    asyncio.create_task(get_page('https://cdn.jsdelivr.net/gh/parserpp/ip_ports@master/proxyinfo.txt',mod=0, session=session)),
-        #    asyncio.create_task(get_page('https://fastly.jsdelivr.net/gh/parserpp/ip_ports@main/proxyinfo.txt',mod=0, session=session)),
-        #    asyncio.create_task(get_page('https://www.proxy-list.download/api/v1/get?type=http', mod=3, session=session)),
+            asyncio.create_task(get_page('http://www.kxdaili.com/dailiip/2/1.html',mod=2, session=session)),
+            asyncio.create_task(get_page('https://www.zdaye.com/free/?ip=&adr=&checktime=3&sleep=2',mod=2, session=session,isUseJs = 1)),
+            asyncio.create_task(get_page('https://www.kuaidaili.com/free', mod=2, session=session,isUseJs = 1)),
+            asyncio.create_task(get_page('https://www.kuaidaili.com/free/fps/', mod=2, session=session,isUseJs = 1)),
+            asyncio.create_task(get_page('http://www.66ip.cn/mo.php?sxb=&tqsl=50',mod=0, session=session)),
+            asyncio.create_task(get_page('https://www.89ip.cn',mod=1, session=session)),
+            asyncio.create_task(get_page('https://cdn.jsdelivr.net/gh/parserpp/ip_ports@master/proxyinfo.txt',mod=0, session=session)),
+            asyncio.create_task(get_page('https://fastly.jsdelivr.net/gh/parserpp/ip_ports@main/proxyinfo.txt',mod=0, session=session)),
+            asyncio.create_task(get_page('https://www.proxy-list.download/api/v1/get?type=http', mod=3, session=session)),
         ]
 
-        #for i in range(1, 4):
-            #task.append(asyncio.create_task(get_page(f'https://proxy.ip3366.net/free/?action=china&page={i}',mod = 2, session=session)))
-            #task.append(asyncio.create_task(get_page(f'http://www.kxdaili.com/dailiip/1/{i}.html',mod=2, session=session)))
-            #task.append(asyncio.create_task(get_page(f'http://ip.tyhttp.com/{i}/',mod=2, session=session)))
+        for i in range(1, 4):
+            task.append(asyncio.create_task(get_page(f'https://proxy.ip3366.net/free/?action=china&page={i}',mod = 2, session=session)))
+            task.append(asyncio.create_task(get_page(f'http://www.kxdaili.com/dailiip/1/{i}.html',mod=2, session=session)))
+            task.append(asyncio.create_task(get_page(f'http://ip.tyhttp.com/{i}/',mod=2, session=session)))
         await asyncio.gather(*task)
 
 
 # 访问网页
 async def get_page(url, session, mod=0 ,isUseJs = 0):
     hd = await getheaders()  # 请求头
-    tout = ClientTimeout(total=30)  # 超时时间
     try:
         if isUseJs:
             page_source = await getHtmlWithRunJs(url)
@@ -174,18 +176,20 @@ async def get_page(url, session, mod=0 ,isUseJs = 0):
             await check_proxtList(ip_lists)
             print(f"['{url}']抓取成功{len(ip_lists)}个")
             return
-        async with await session.get(url=url, headers=hd, timeout=tout) as response:
+        async with await session.get(url=url, headers=hd, timeout=20) as response:
             data = await response.read()  # 返回字符串形式的相应数据
             encoding = chardet.detect(data)['encoding']
-            print(f'page detected  possible encoding:{encoding}')
+            #print(f'page detected  possible encoding:{encoding}')
             encodings = [encoding ,'utf-8', 'gbk', 'GB2312', 'windows-1252']  # 常见编码列表
             for encoding in encodings:
                 try:
                     page_source = data.decode(encoding)
-                    print(page_source)
+                    #print(page_source)
                     ip_lists = await soup_page(page_source, mod=mod)
-                    await check_proxtList(ip_lists)
-                    print(f"['{url}']成功解码：{encoding}")
+                    #await check_proxtList(ip_lists)
+                    for ip in ip_lists:
+                        await record(ip)
+                    #print(f"['{url}']成功解码：{encoding}")
                     print(f"['{url}']抓取成功{len(ip_lists)}个")
                     break
                 except UnicodeDecodeError:
@@ -207,7 +211,6 @@ async def soup_page(source, mod):
         ip_port = re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}\b', source)
         for i in ip_port:
             ip_list.append(f"http://{i}")
-        return ip_list
 
     elif mod == 1:
         # 89ip.cn
@@ -215,22 +218,21 @@ async def soup_page(source, mod):
         posts = re.findall(r'\s(\d{1,5})\s', source)
         for i in range(len(ips)):
             ip_list.append(f"http://{ips[i]}:{posts[i]}")
-        return ip_list
 
     elif mod == 2:
         # 快代理 || kxdaili.com || proxy.ip3366.net || http://ip.tyhttp.com/3/
-        ips = re.findall(r'>(\d+\.\d+\.\d+\.\d+)</td>', source)
-        posts = re.findall(r'>(\d{1,5})</td>', source)
+        ips = re.findall(r'>(\d+\.\d+\.\d+\.\d+)</', source)
+        posts = re.findall(r'>(\d{2,5})</', source)
         for i in range(len(ips)):
             ip_list.append(f"http://{ips[i]}:{posts[i]}")
-        return ip_list
 
     elif mod == 3:
         # www.proxy-list.download/api/v1/get?type=http
         ip_port = source.split('\r\n')[:-1]
         for i in ip_port:
             ip_list.append(f"http://{i}")
-        return ip_list
+    random.choices(ip_list)
+    return ip_list
 
 
 
@@ -242,3 +244,4 @@ def ip_main():
     loop.run_until_complete(create_aiohttp())
     print("代理抓取完成!!!")
 
+#ip_main()
--- Momo/main.py
+++ Momo/main.py
@@ -1,7 +1,10 @@
 # encoding:utf-8
 import asyncio
+import sys
+import os
 from aiohttp import ClientSession, ClientTimeout, TCPConnector
 from ip import ip_main, getheaders
+import random
 
 # 可根据自己电脑更改位置
 link_path = "/var/www/tools/momo-share/momo-share-proxy/Momo/momo_link.txt"
@@ -16,19 +19,16 @@ def readfile():
 
 # 读取文件获取momo链接
 def share_Link():
-    import sys
-    import os
-
     # 判断文件是否存在，不存在则创建
     if os.path.isfile(link_path):
         fileopen = open(link_path, 'r', encoding='utf-8')
-        momo_share_link = fileopen.readline().strip()
+        momo_share_links = [i.strip() for i in fileopen.readlines()]
         # 判断是否有链接 无则终止程序
-        if momo_share_link == '':
+        if momo_share_links == []:
             fileopen.close()
             sys.exit()
         fileopen.close()
-        return momo_share_link.strip()
+        return momo_share_links
     else:
         os.close(os.open(link_path, os.O_CREAT))  # 创建文件
         sys.exit()  # 终止程序
@@ -39,9 +39,7 @@ def share_Link():
 
 
 async def create_aiohttp(url, proxy_list):
-    global n
-    n = 0
-
+    random.choices(proxy_list)
     async with ClientSession(connector=TCPConnector(ssl=False)) as session:
         # 生成任务列表
         task = [asyncio.create_task(web_request(url=url, proxy=proxy, session=session))
@@ -51,20 +49,28 @@ async def create_aiohttp(url, proxy_list):
 
 # 网页访问
 async def web_request(url, proxy, session):
+    global n 
+    global cnt
+    if n>25:
+        print("n:",n)
+        return
+    print(f'次数{cnt}:{proxy}')
+    cnt +=1
     hd = await getheaders()  # 设置请求头
     sem = asyncio.Semaphore(50)  # 设置限制并发次数.
-    tout = ClientTimeout(total=20)  # 设置请求超时
     async with sem:
         try:
-            async with await session.get(url=url, headers=hd, proxy=proxy, timeout=tout) as response:
+            async with await session.get(url=url, headers=hd, proxy=proxy, timeout=10) as response:
                 # 返回字符串形式的相应数据
                 page_source = await response.text()
                 await page(page_source)
         except Exception as e:
-            print("网页请求失败! ", e)
+            #print("网页请求失败! ", e)
+            pass
 
 
 global n  # 记录访问成功次数
+global cnt
 
 
 # 判断访问是否成功
@@ -73,24 +79,22 @@ async def page(page_source):
     if "学习天数" in page_source:
         n += 1
         print('访问成功!!!')
-    else:
-        print('访问失败! 页面无此元素。')
 
 
 def main():
-    link = share_Link()  # 读取文件里的墨墨分享链接
-    print("访问链接:", link)
+    links = share_Link()  # 读取文件里的墨墨分享链接
+    print("访问链接:", links)
     #ip_main()  # 抓取代理
-    proxies = [i.strip() for i in readfile()]  # 生成代理列表
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(create_aiohttp(link, proxies))
+    proxies = [i.strip() for i in readfile()] # 生成代理列表
+    for link in links:
+        global n
+        global cnt
+        n = 0
+        cnt = 0
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(create_aiohttp(link, proxies))
+        print(f'[url:{link}]完成{n}次')
     print("任务完成!!!")
-
-    # 呈现程序运行结果。可根据自己电脑更改位置
-    reminder_path = f'访问成功{n}次.txt'
-    # 创建运行结果提示文件
-    with open(reminder_path, 'w', encoding='utf-8') as f:
-        f.close()
 
 
 if __name__ == '__main__':
